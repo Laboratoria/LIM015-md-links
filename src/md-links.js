ab6fd9d58d98ed7d6971e94535f5c9f2d8f74032
margaZM <aurazambrano227@gmail.com>
2021-08-02 18:19:49 -0500

    Agrega funcion md-links

---
+++ src/md-links.js
@@ -0,0 +1,20 @@
+#!/usr/bin/env node 
+const index = require('./index')
+// const [,, route] = process.argv;
+// const routeAbsolute = index.validateRoute(route);
+// console.log(routeAbsolute)
+// const allFiles = index.listFiles(routeAbsolute );
+// console.log(allFiles)
+// const onlyMd = index.listFilesMd(allFiles);
+// console.log(onlyMd)
+// const urlData = index.checkLinks(onlyMd)
+// console.log(urlData)
+
+// const mdLinks = (path, options) => {
+
+// }
+
+// const onlyMd = index.typePath(route);
+// // console.log(onlyMd)
+// const list = index.listFiles(route)
+// console.log(list)
