1bb6ee2184b3a9914354c2f0a11cb3a94fbb2048
NINOSKA  NATALIA ESPINOZA BARRIENTOS <ninoska133333@gmail.com>
2021-08-10 16:36:23 -0500

    convertir un archivo a direc

--- index.js
+++ index.js
@@ -1,3 +1,70 @@
 module.exports = () => {
-  // ...
-};
+}
+
+
+const path = require('path');
+const  fs = require('fs');
+
+
+//evaluar si la ruta es absoluta o relatuiva 
+
+function absolutePath(ruta){
+
+const resultPath=path.isAbsolute(ruta);
+  return  resultPath;
+
+}
+
+//convertir mi ruta relativa absoluta 
+
+function convertedPath(ruta){
+
+  const convertedpath=path.resolve(ruta);
+    return  convertedpath;
+}
+
+
+// saber si es un archivo o directorio 
+
+function fileExists(ruta){ 
+
+  try { 
+
+      if(fs.statSync(ruta).isFile()) {
+        return "es archivo";
+       }
+        
+      else if  (fs.statSync(ruta).isDirectory()) {
+        return "es directorio";
+       }
+       
+    } 
+    catch (err)
+    {
+        return "no existe";
+    }    
+     
+      }
+
+
+ //convertir un directorio en un archivo 
+
+
+ let data=fs.readFileSync("texto.txt","utf-8");
+ console.log(data);
+
+
+
+
+
+//console.log(absolutePath("D:\\PROGRAMACION\\LIM015-md-links\\data.txt"));
+//console.log(convertedPath("function.js"));
+//console.log(fileExists("D:\\PROGRAMACION\\LIM015-md-links\\data.txt"));
+
+
+
+
+
+
+
+
