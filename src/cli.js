db21bcfd20e322cdf8f45ef2e0bd002caed436cc
BethzyOrtega <bethzykathia@gmail.com>
2021-09-01 10:27:48 -0500

    intalacion de color y aplicarle color. Close #13

--- src/cli.js
+++ src/cli.js
@@ -1,5 +1,6 @@
 const { mdLinks } = require('./mdlinks');
 const option = require('./option');
+const colors = require('colors');
 // const { totalAndUnique, totalLinksBroken } = require('./option');
 
 // console.log(process.argv);
@@ -8,7 +9,7 @@ const argument = process.argv.slice(2);
 
 if (argument.length === 1) {
   mdLinks(argument[0], { validate: false })
-    .then(res => res.forEach( el => console.log(`${el.file} ${el.href} ${el.text}`)))
+    .then(res => res.forEach( el => console.log(`${colors.green(el.file)} ${colors.rainbow(el.href)} ${colors.brightMagenta(el.text)}`)))
     .catch(err => console.log(err));
 }
 
@@ -16,34 +17,34 @@ if (argument.length === 2) {
   switch (argument[1]) {
   case '--validate':
     mdLinks(argument[0], { validate: true })
-      .then(res => res.forEach(el => console.log(`${el.file} ${el.href} ${el.message} ${el.status} ${el.text}`)))
+      .then(res => res.forEach(el => console.log(`${colors.green(el.file)} ${colors.rainbow(el.href)} ${colors.yellow(el.message)} ${colors.yellow(el.status)} ${colors.brightMagenta(el.text)}`)))
       .catch(err => console.log(err));
     break;
   
   case '--stats':
     mdLinks(argument[0], { validate: true })
-      .then(res => console.log(option.totalAndUnique(res)))
+      .then(res => console.log(colors.brightMagenta(option.totalAndUnique(res))))
       .catch(err => console.log(err));
     break;
   
   case '--help':
     console.log(`
     --------------------------------------------------------------------------------------------------------------
-    ('--validate') muestra la ruta, enlace, numero de status, status cada link (OK o FAIL) y el texto
-    ('--stats') muestra el número total de links y los links unicos(no se repiten).      
-    ('--stats --validate') muestra el total de links, únicos y rotos.
-    *Nota : Si no ingresa nunguna opcion, solo debe ingresar la ruta y obtendra como resultado la ruta, el link y el texto del archivo.
+    ${colors.yellow('--validate')} muestra la ruta, enlace, numero de status, status cada link (OK o FAIL) y el texto
+    ${colors.yellow('--stats')} muestra el número total de links y los links unicos(no se repiten).      
+    ${colors.yellow('--stats --validate')} muestra el total de links, únicos y rotos.
     --------------------------------------------------------------------------------------------------------------
+    ${colors.green('Nota: Si no ingresa ninguna opcion, solo debe ingresar la ruta y obtendra como resultado la ruta, el link y el texto del archivo.')}    
     `);
     break;
     
-  default: console.log('Lo siento, no es un comando válido. Intente con la opcion --help');
+  default: console.log(colors.brightRed('Lo siento, no es un comando válido. Intente con la opcion --help'));
     break;
   }
 }
 
 if (argument.length === 3) {
   mdLinks(argument[0], { validate: true })
-    .then(res => console.log(`${option.totalAndUnique(res)}\n${option.totalLinksBroken(res)}`))
+    .then(res => console.log(`${colors.brightMagenta(option.totalAndUnique(res))}\n${colors.red(option.totalLinksBroken(res))}`))
     .catch(err => console.log(err));
 }
--- src/prueba/prueba1.md
+++ src/prueba/prueba1.md
@@ -1,4 +1,4 @@
-[Import Export](https://developer.mozilla.org/es/docs/Web/JavaScript/Reference/Statements/export)
+[Moks](https://jestjs.io/es-ES/docs/manual-mocks)
 [Regex](https://regexr.com/)
 [Regex](https://regexr.com/)
 [figma](https://www.figma.com/blog/1)
