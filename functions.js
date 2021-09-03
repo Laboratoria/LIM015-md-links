0997bc1411c810237f936ce08976bb2b1dd7b44d
nadiacontreras-code <nadiabrenda.cs@gmail.com>
2021-09-03 11:41:33 -0500

    creando funciones pequeñas

--- functions.js
+++ functions.js
@@ -2,21 +2,87 @@ const fs = require('fs');
 const path = require('path');
 
 //----****----****--- LEYENDO CONTENIDO COMO ARHCIVO-****----**---** //
-const pathToFile = process.argv[2];
-//console.log(pathToFile);
-//pathToFile = './prueba/firstFile/readme.md';
+const pathTest = process.argv[2];
+//console.log(pathTest);
 
-function pathToRead (){
-// Use fs.readFile() method to read the file
-fs.readFile('Demo.txt', 'utf8', function(err, data){
-    console.log(data);
-});
-  
-//console.log('readFile called');
+const pathExistFun = (pathPrueba) => {
+    try{
+        const pathExist2 = fs.existsSync(pathPrueba);
+            //console.log(`pathExistFun exist ${pathExist2}`);
+        return pathExist2
+    }catch(err){
+        console.log(err.message);
+    }
 }
-//pathToRead()
+ //pathExistFun(pathTest);*/
 
-function fileToRead(path){
+ const pathIsAbsolute = (pathPrueba) => {
+    const pathIsAbsolute = path.isAbsolute(pathPrueba);
+     //console.log(`pahtAbsolute is ${pathIsAbsolute}`);
+    return pathIsAbsolute
+   
+}
+//pathIsAbsolute(pathTest)
+
+const pathResolveAbsolute = (pathPrueba) => {
+    try{
+       const pathToAbs = path.resolve(pathPrueba); 
+     //
+        console.log(`pathToAbs ${pathToAbs}`);
+       return pathToAbs;
+    }catch (e) {
+        console.log(e.message)
+    }
+}
+pathResolveAbsolute(pathTest);
+
+const pathIsFile = (pathPrueba) => {
+    const readFile = fs.statSync(pathPrueba)
+    console.log(readFile.isFile())
+    return  readFile.isFile()
+}
+pathIsFile(pathTest)
+
+const pathIsDirectory = (pathPrueba) => {
+    const directoryContent = fs.readdirSync(pathPrueba)
+        //console.log(directoryContent);
+        const containerDir =[];
+        directoryContent.forEach(file => {
+        containerDir.push(file)
+         /* if (path.extname(file) == ".md") // CONOCIENDO EL TIPO DE EXTENSIÓN //
+        console.log(file)*/
+   })
+   console.log(containerDir)
+   return containerDir
+   
+}
+//pathIsDirectory(pathTest)
+// [ 'firstFile', 'indexPrueba.js', 'read.txt', 'rutaNotas.md' ]
+
+const fileExtention = (pathPrueba)=>{
+
+    const pathArrayDir =pathIsDirectory(pathPrueba); //[ 'firstFile', 'indexPrueba.js', 'read.txt', 'rutaNotas.md' ]
+    const containerExt =[];
+    for(let i=0; i< pathArrayDir.length; i++){
+        console.log(pathArrayDir[i]) 
+    const extension = path.extname(pathArrayDir[i])
+        if (extension == '.md'){
+            containerExt.push(pathArrayDir[i])
+        //}else{//extention !== '.md'
+          //  const extNoMd = [];
+          //  extNoMd.push(pathArrayDir[i])
+           // console.log(extNoMd)
+       //console.log('it does a file but it does not a markdown')
+       // return 'it does a file but it does not a markdown'
+        }
+    }
+   //console.log(containerExt)
+   //return containerExt
+}
+//fileExtention(pathTest)
+
+
+const pathToRead = (path)=> {
     try{
         //if (fs.statSync(path).isFile()) {
         fs.readFile(path,'utf8', (err, data) => {
@@ -31,22 +97,25 @@ function fileToRead(path){
 function directoyToRead (path){
     try{
         fs.readdir(path,'utf8',(err, data) => {
-       // if (err) {
-       //  console.log(err.message);
-       // } else{
         console.log(`reading directory: `);
-        //let result = data;
+        
         console.log(data)
-        //}
         });
     } catch (err) {
         console.log(err.message);
     }
 }
 
-
-module.exports = {
-    fileToRead,
-    directoyToRead,
-    //existPath
+module.exports ={
+    pathExistFun,
+    pathIsAbsolute,
+    pathResolveAbsolute,
+    pathIsFile,
+    pathIsDirectory,
+    fileExtention
+    
 }
+//}*/
+
+  
+ 
