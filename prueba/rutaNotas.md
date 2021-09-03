c6805c89c86f17f05f65ca4bdfe3f5da54677865
nadiacontreras-code <nadiabrenda.cs@gmail.com>
2021-09-03 12:04:20 -0500

    creando archivo para prueba y pasando test

--- prueba/rutaNotas.md
+++ prueba/rutaNotas.md
@@ -1,1 +1,3 @@
 Creando archivos de prueba
+ - [Promise - MDN](https://developer.mozilla.org/es/docs/Web/JavaScript/Reference/Global_Objects/Promise)
+  - [How to Write a JavaScript Promise - freecodecamp (en inglés)](https://www.freecodecamp.org/news/how-to-write-a-javascript-promise-4ed8d44292b8/)
--- test/md-links.spec.js
+++ test/md-links.spec.js
@@ -1,10 +1,61 @@
-const mdLinks = require('../');
+//const {mdLinks} = require('../index');
+const{ pathExistFun, pathIsAbsolute, pathResolveAbsolute, pathIsFile} = require('../functions')
 
-
-describe('mdLinks', () => {
+const pathSpec = 'text.txt';
+const pathDir = './prueba/'
+const arrayList = [ 'firstFile', 'indexPrueba.js', 'read.txt', 'rutaNotas.md' ];
+/*describe('mdLinks', () => {
 
   it('should...', () => {
     console.log('FIX ME!');
   });
 
+});*/
+describe('pathExistFun', () => {
+  it('should be a function', () => {
+    expect(typeof pathExistFun).toBe('function');
+  });
+  test('should be a exist path ', () => {
+    expect(pathExistFun(pathSpec)).toBe(true);
+  });
+
 });
+
+describe('pathIsAbsolute', () => {
+  it('should be a function', () => {
+    expect(typeof pathIsAbsolute).toBe('function');
+  });
+  test('should be a Absolute path ', () => {
+    expect(pathIsAbsolute(pathSpec)).toBe(false);
+  });
+
+});
+
+describe('pathResolveAbsolute', () => {
+  it('should be a function', () => {
+    expect(typeof pathResolveAbsolute).toBe('function');
+  });
+  test('should convert to absolute path ', () => {
+    expect(pathResolveAbsolute(pathSpec)).toBe('C:\\Users\\nadia\\Documents\\GitHub\\MDLink\\LIM015-md-links2\\text.txt');
+  });
+});
+
+describe('pathIsFile', () => {
+  it('should be a function', () => {
+    expect(typeof pathIsFile).toBe('function');
+  });
+  test('should show a file list ', () => {
+    expect(pathIsFile(pathSpec)).toBe(true); //toStrictEqual
+    expect(pathIsFile(pathDir)).toBe(false); //toStrictEqual
+  });
+});
+
+/*describe('pathIsDirectory', () => {
+  it('should be a function', () => {
+    expect(typeof pathIsFile).toBe('function');
+  });
+  test('should show a file list ', () => {
+    expect(pathIsDirectory(pathSpec)).toStrictEqual(true); //toStrictEqual
+    expect(pathIsFile(pathSpec)).toStrictEqual(true); //toStrictEqual
+  });
+});*/
