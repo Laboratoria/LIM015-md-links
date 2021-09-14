e328b5c7a9834dc4dac3bd0eea2ae612fdade28e
Andrea Blanco <estefania_8_3@hotmail.com>
2021-09-13 21:37:01 -0500

    docs: agregue readme, archivos y configuraciones

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
+const chalk = require('chalk');
+console.log(chalk.blue.bgRed.bold('Hello world!'));
+
+console.log(process.argv[1]); //indica la ruta
 module.exports = () => {
-  // ...
+  console.log('hola mundo');
 };
