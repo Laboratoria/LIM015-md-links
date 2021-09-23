34c0d297009d66253d2a6c7733ea381dcf344a37
RocioSulca <rociosulca.z@gmail.com>
2021-09-22 22:11:21 -0500

    Funcion cli

--- src/cli.js
+++ src/cli.js
@@ -1,11 +1,14 @@
 #!/usr/bin/env node
 
-const arg = process.argv[2];
 
 // const commander = require('commander');
 
 const { mdLinks } = require('./index.js');
 const { uniqueLink, brokenLink, totalLink } = require('./ruta.js');
+const { helpMsg, mdLiMsg, errMsg } = require('./msg.js');
+
+const arg = process.argv[2];
+console.log(mdLiMsg);
 
 // const program = new commander.Command();
 // program.version('0.0.1');
@@ -74,30 +77,33 @@ const [, , ...args] = process.argv;
 if (args.length === 1) {
     mdLinks(arg, { validate: false }).then(resolve => {
         console.log(resolve);
-    }).catch(reject => console.log(reject));
+    }).catch(reject => console.log(errMsg, reject));
 }
 
 if (args.length === 2) {
     if (args[1] === '--stats') {
         mdLinks(arg, { validate: true }).then((resolve) => console.log(totalLink(resolve) + uniqueLink(resolve)))
-            .catch((err) => console.log(err));
+            .catch((err) => console.log(errMsg, err));
     };
     if (args[1] === '--validate') {
         mdLinks(arg, { validate: true }).then(resolve => {
                     console.log(resolve);
-                }).catch(reject => console.log(reject));
+                }).catch(reject => console.log(errMsg, reject));
+    };
+    if(args[1] === '--help') {
+        console.log(helpMsg);
     };
 };
 
 if (args.length === 3) {
     if ((args[1] === '--stats' && args[2] === '--validate') || (args[1] === '--validate' && args[2] === '--stats')) {
         mdLinks(arg, { validate: true }).then((resolve) => console.log(totalLink(resolve) + uniqueLink(resolve) + brokenLink(resolve)))
-            .catch((err) => console.log(err));
+            .catch((err) => console.log(errMsg, err));
     };
 };
-// if (args[1] !== '--stats' && args[1] !== '--validate' && args[1] !== null) {
-//     console.log('no se encontro comando valido')
-// };
+if (args[1] !== '--stats' && args[1] !== '--validate' && args[1] !== null) {
+    console.log('No se encontró comando válido, pruebe con: --help')
+};
 
 
 
--- src/md-linksCli.js
+++
@@ -1,5 +0,0 @@
-// const { mdLinksCli } = require('../src/cli.js');
-
-// const [,, path, option1, option2] = process.argv;
-
-// mdLinksCli(path, option1, option2).then((result) => console.log(result));
