daab60c307c7793e65ed67ef0fed037d26612330
mysticBel <dnxbel@gmail.com>
2021-09-28 05:05:29 -0500

    v1 api functions + test.md -eslint'standard'config

--- index.js
+++ index.js
@@ -1,3 +1,50 @@
-module.exports = () => {
-  // ...
+const path = require('path');
+const fs = require('fs');
+
+const convertToAbsolutePath = (inputPath) => {
+  if (path.isAbsolute(inputPath)){
+      return inputPath;
+  } else{
+      return path.resolve(inputPath);
+  }
 };
+
+statsObj = fs.statSync('./prueba/test.md');
+
+const checkTypeObject = (inputPath) => {
+    let arrayPathFiles = [];
+    if (fs.statSync(inputPath).isDirectory()) {
+      const readDirectory = fs.readdir(inputPath, (err,file) => {
+          if (err) {
+              return console.error(err);
+          }
+      })
+     readDirectory.forEach(file => {
+         const pathFile = path.join(inptPath, file);
+         arrayPathFiles = arrayPathFiles.concat(checkTypeObject(pathFile))
+     })
+     return arrayPathFiles;
+
+    } else {
+        return console.log('it is a file', convertToAbsolutePath(inputPath))
+    }
+};
+// console.log(checkTypeObject("LIM015-md-links\prueba\test.md"));
+console.log(statsObj);
+
+
+
+
+
+
+
+
+
+
+
+
+// module.exports = () => {
+//   // ...
+// };
+
+// console.log("henlo :P");
---
+++ prueba/test.md
@@ -0,0 +1,11 @@
+**These pages are stored in my search history 👁️👄👁️🖥️**
+
+this is link one :  [UX library](https://www.uxlibrary.org/explore/ui-design)
+this is link two : [Yoshi web page](https://yoshiscraftedworld.nintendo.com)
+this is link three : [Steamed Hams but it's Basket Case by Green Day🎵 ](https://youtu.be/vqCHdVOzetc)
+this is link four : [Unsettling Things You Didn't Know About the Smurfs](https://youtu.be/fGVtIpaqaVk)
+and
+this is link five : [(HQ) Vitas - The 7th Element 2001🎵](https://youtu.be/qWVc-xVZxho)
+<!-- 
+C:\Users\LORD\Desktop\mdlinks-prueba\LIM015-md-links\prueba\test.md
+LIM015-md-links\prueba\test.md -->
